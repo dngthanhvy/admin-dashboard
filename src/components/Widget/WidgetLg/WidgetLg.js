0f1bdd9830e183998f8e8671eef2b127add8d6ea
Thanh-Vy Duong <dng.thanhvy@gmail.com>
2021-07-07 17:25:23 +0200

    add: latest transactions widget

--- src/components/Widget/WidgetLg/WidgetLg.js
+++ src/components/Widget/WidgetLg/WidgetLg.js
@@ -1,9 +1,69 @@
+import { Avatar } from '@material-ui/core';
+
 import './WidgetLg.css'
 
 export default function WidgetLg() {
+
+    const CustomButton = ({type}) => {
+        return <button className={"widgetLg__btn " + type}>{type}</button>
+    }
+
     return (
         <div className="widgetLg">
-            WidgetLg
+            <h3 className="widgetLg__title"> Latest Transactions </h3>
+
+            <table className="widgetLg__table">
+                <tr className="widgetLg__tr">
+                    <th className="widgetLg__th">Customer</th>
+                    <th className="widgetLg__th">Date</th>
+                    <th className="widgetLg__th">Amount</th>
+                    <th className="widgetLg__th">Status</th>
+                </tr>
+
+                <tr className="widgetLg__tr">
+                    <td className="widgetLg__user">
+                        <Avatar className="widgetLg__userAvatar"/>
+                        <span className="widgetLg__userName">Susan Carol</span>
+                    </td>
+
+                    <td className="widgetLg__date">7 July 2021</td>
+                    <td className="widgetLg__amount">$122.00</td>
+                    <td className="widgetLg__status"><CustomButton type="Approved"/></td>
+                </tr>
+
+                <tr className="widgetLg__tr">
+                    <td className="widgetLg__user">
+                        <Avatar className="widgetLg__userAvatar"/>
+                        <span className="widgetLg__userName">Susan Carol</span>
+                    </td>
+
+                    <td className="widgetLg__date">7 July 2021</td>
+                    <td className="widgetLg__amount">$122.00</td>
+                    <td className="widgetLg__status"><CustomButton type="Declined"/></td>
+                </tr>
+
+                <tr className="widgetLg__tr">
+                    <td className="widgetLg__user">
+                        <Avatar className="widgetLg__userAvatar"/>
+                        <span className="widgetLg__userName">Susan Carol</span>
+                    </td>
+
+                    <td className="widgetLg__date">7 July 2021</td>
+                    <td className="widgetLg__amount">$122.00</td>
+                    <td className="widgetLg__status"><CustomButton type="Pending"/></td>
+                </tr>
+
+                <tr className="widgetLg__tr">
+                    <td className="widgetLg__user">
+                        <Avatar className="widgetLg__userAvatar"/>
+                        <span className="widgetLg__userName">Susan Carol</span>
+                    </td>
+
+                    <td className="widgetLg__date">7 July 2021</td>
+                    <td className="widgetLg__amount">$122.00</td>
+                    <td className="widgetLg__status"><CustomButton type="Approved"/></td>
+                </tr>
+            </table>
         </div>
     )
 }
